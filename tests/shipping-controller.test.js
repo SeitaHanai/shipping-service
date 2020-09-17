339c8d6765131ce8517a712cafa7a088243e8622
Seita Hanai <Seita.Hanai@ibm.com>
2020-09-17 23:18:44 +0900

    a

--- tests/shipping-controller.test.js
+++ tests/shipping-controller.test.js
@@ -1,5 +1,20 @@
-// File tests/shipping-controller.test.js
-describe('Shipping Controller', function() {
-    // Test cases will go here
-    it('Canary test', () => {})
+/**
+* @jest-environment node
+*/
+var productService = require('../src/services/product-service')
+var nock = require('nock')
+
+describe('Product service', function () {
+  it('Should call remote service', async function () {
+    nock('https://product.service:8899/products')
+      .get('/13')
+      .delayBody(10)
+      .reply(200, {
+        weightLB: 15.5,
+        unit: 'lbs'
+      })
+
+    let weight = await productService.getProductWeight('13')
+    expect(weight).toBe(15.5)
+  })
 })
